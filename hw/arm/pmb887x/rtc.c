5ed79c45a2d4c3e9650e09dd3d11806a14f67128
Alula <git@alula.me>
2026-06-21 11:04:07 +0200

    rtc fix

--- hw/arm/pmb887x/rtc.c
+++ hw/arm/pmb887x/rtc.c
@@ -40,10 +40,100 @@ struct pmb887x_rtc_t {
 	uint32_t isnc;
 	uint32_t alarm;
 	uint32_t unk0;
-	
+
 	uint64_t realtime_start;
 	uint64_t virtual_start;
+
+	/* Packed RTC_CNT modelling: the firmware seeds RTC_CNT/RTC_REL with a
+	 * packed S-Gold2 section counter and the hardware then auto-advances it.
+	 * We capture the last firmware-written seed and the virtual time at which
+	 * it was written, then re-derive RTC_CNT on every read. */
+	uint32_t cnt_seed;			/* packed value last written by firmware */
+	uint64_t cnt_seed_ns;		/* QEMU_CLOCK_VIRTUAL ns at that write */
+
+	QEMUTimer *tick;
 };
+
+/*
+ *   bits [31:22] CNT3 = day-of-year field, encoded (1024 - yeardays + doy - 1)
+ *   bits [21:16] CNT2 = hours   (field value - 40, range 0..23)
+ *   bits [15:10] CNT1 = minutes (field value - 4,  range 0..59)
+ *   bits [9:0]   CNT0 = seconds (field value - 964, range 0..59)
+ */
+#define RTC_YEARDAYS 365
+
+static uint32_t rtc_cnt_encode(uint32_t secs_in_year) {
+	uint32_t sec = secs_in_year % 60;
+	uint32_t min = (secs_in_year / 60) % 60;
+	uint32_t hour = (secs_in_year / 3600) % 24;
+	uint32_t doy = (secs_in_year / 86400) + 1;	/* 1-based */
+	uint32_t cnt3 = (1024 - RTC_YEARDAYS + doy - 1) & 0x3FF;
+	return (cnt3 << 22)
+		| (((hour + 40) & 0x3F) << 16)
+		| (((min + 4) & 0x3F) << 10)
+		| ((sec + 964) & 0x3FF);
+}
+
+static uint32_t rtc_cnt_decode(uint32_t cnt) {
+	int32_t sec = (int32_t)(cnt & 0x3FF) - 964;
+	int32_t min = (int32_t)((cnt >> 10) & 0x3F) - 4;
+	int32_t hour = (int32_t)((cnt >> 16) & 0x3F) - 40;
+	int32_t cnt3 = (int32_t)((cnt >> 22) & 0x3FF);
+	int32_t doy = RTC_YEARDAYS - (1024 - cnt3) + 1;	/* 1-based */
+
+	if (sec < 0 || sec > 59) sec = 0;
+	if (min < 0 || min > 59) min = 0;
+	if (hour < 0 || hour > 23) hour = 0;
+	if (doy < 1 || doy > RTC_YEARDAYS) doy = 1;
+
+	return (uint32_t)((doy - 1) * 86400 + hour * 3600 + min * 60 + sec);
+}
+
+static uint32_t rtc_cnt_now(pmb887x_rtc_t *p) {
+	uint64_t now = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
+	uint64_t elapsed = (now - p->cnt_seed_ns) / NANOSECONDS_PER_SECOND;
+	uint32_t base = rtc_cnt_decode(p->cnt_seed);
+	uint32_t total = (uint32_t)((base + elapsed) % (RTC_YEARDAYS * 86400ULL));
+	return rtc_cnt_encode(total);
+}
+
+#define RTC_TICK_HZ 1
+
+static void rtc_tick_rearm(pmb887x_rtc_t *p) {
+	timer_mod(p->tick, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) +
+		NANOSECONDS_PER_SECOND / RTC_TICK_HZ);
+}
+
+static void rtc_tick(void *opaque) {
+	pmb887x_rtc_t *p = opaque;
+
+	uint32_t cnt = rtc_cnt_now(p);
+	uint32_t set = 0;
+
+	/* Set the request (IR) flag for every interrupt source the firmware has
+	 * enabled (IE). IR bit is the IE bit << 1. The LISR recomputes the
+	 * actual time fields from RTC_CNT, so signalling on every second is
+	 * safe regardless of which section actually rolled over. */
+	if ((p->isnc & RTC_ISNC_T14IE))
+		set |= RTC_ISNC_T14IR;
+	if ((p->isnc & RTC_ISNC_RTC0IE))
+		set |= RTC_ISNC_RTC0IR;
+	if ((p->isnc & RTC_ISNC_RTC1IE))
+		set |= RTC_ISNC_RTC1IR;
+	if ((p->isnc & RTC_ISNC_RTC2IE))
+		set |= RTC_ISNC_RTC2IR;
+	if ((p->isnc & RTC_ISNC_RTC3IE))
+		set |= RTC_ISNC_RTC3IR;
+	if ((p->isnc & RTC_ISNC_ALARMIE) && p->alarm && cnt >= p->alarm)
+		set |= RTC_ISNC_ALARMIR;
+
+	if (set) {
+		p->isnc |= set;
+		pmb887x_src_update(&p->src, 0, MOD_SRC_SETR);
+	}
+
+	rtc_tick_rearm(p);
+}
 
 static uint64_t rtc_io_read(void *opaque, hwaddr haddr, unsigned size) {
 	pmb887x_rtc_t *p = opaque;
@@ -72,7 +162,7 @@ static uint64_t rtc_io_read(void *opaque, hwaddr haddr, unsigned size) {
 			break;
 		
 		case RTC_CNT:
-			value = get_clock_realtime() / NANOSECONDS_PER_SECOND;
+			value = rtc_cnt_now(p);
 			break;
 		
 		case RTC_REL:
@@ -130,6 +220,8 @@ static void rtc_io_write(void *opaque, hwaddr haddr, uint64_t value, unsigned si
 		
 		case RTC_CNT:
 			p->cnt = value;
+			p->cnt_seed = value;
+			p->cnt_seed_ns = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
 			break;
 		
 		case RTC_REL:
@@ -183,9 +275,15 @@ static void rtc_realize(DeviceState *dev, Error **errp) {
 	
 	p->virtual_start = qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL);
 	p->realtime_start = qemu_clock_get_ns(QEMU_CLOCK_REALTIME);
-	
+
+	p->cnt_seed = 0xA4D9F9C4;
+	p->cnt_seed_ns = p->virtual_start;
+
 	pmb887x_clc_init(&p->clc);
 	pmb887x_src_init(&p->src, p->irq);
+
+	p->tick = timer_new_ns(QEMU_CLOCK_VIRTUAL, rtc_tick, p);
+	rtc_tick_rearm(p);
 }
 
 static void rtc_class_init(ObjectClass *klass, const void *data) {
